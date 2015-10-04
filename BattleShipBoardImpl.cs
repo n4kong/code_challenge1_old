70dfdfb298f537cd3461ee4ac5f4a3c034026ceb
sunpawets <sunpawet.somsin@thomsonreuters.com>
2015-10-04 11:02:48 +0700

    add CodeChallenge1.csproj

---
+++ BattleShipBoardImpl.cs
@@ -0,0 +1,51 @@
+﻿using Board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge1
+{
+    public class BattleShipBoardImpl : BattleShipBoard
+    {
+        public override void Play()
+        {
+            // Implement AI logic here
+
+            #region example code 
+            /*
+            var isCompleted = false;
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    var result = Fire(i, j);
+                    PrintBoard();
+                    if (result == Result.COMPLETED)
+                    {
+                        isCompleted = true;
+                        break;
+                    }
+                }
+                if (isCompleted)
+                    break;
+            }
+             */
+            #endregion
+        }
+
+        public void PrintBoard()
+        {
+            Console.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(board[i, j] ?? ".");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BattleShipBoardImpl board = new BattleShipBoardImpl();
+            board.Delay = 50;
+
+
+            board.NewGame1();
+            board.PrintBoard();
+            board.Play();
+            Console.WriteLine("Total Hit: " + board.HitCount + ", Enter to start next game.");
+            Console.ReadLine();
+
+            board.NewGame2();
+            board.PrintBoard();
+            board.Play();
+            Console.WriteLine("Total Hit: " + board.HitCount + ", Enter to start next game.");
+            Console.ReadLine();
+
+            board.NewGame3();
+            board.PrintBoard();
+            board.Play();
+            Console.WriteLine("Total Hit: " + board.HitCount);
+            Console.ReadLine();
+        }
+
+
+    }
+
+   
+}
